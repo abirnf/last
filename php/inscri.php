62eefe5506af1e3b52134406b08378a9831ce033
Abir Nefizi <abir.nefzi@outlook.com>
2019-03-30 10:01:28 +0100

    testinii

--- php/inscri.php
+++ php/inscri.php
@@ -1,9 +1,23 @@
 <?php
 //include fichier
-include "confg.php";
+include "config.php";
 //appel fonction
 $base = connect();
 //recuperation des donnees
 $e= $_REQUEST['email'];
 $m= $_REQUEST['mdp'];
+
+$req ="INSERT INTO users (id,email,password) VALUES (null,'$e','$m')";
+//exec (insert,update,delete)--> type de retour (int/bool)
+//int=nbr ligne inserer (itha rq saret b shiih)
+//bool= avec la valeur false en cas d'echec
+$resp=$base->exec($req);
+if ($resp==1)
+{
+    echo "donnees inserer w jawhom behi";
+}
+else
+{
+echo "barra thabet f req wala fl fichier config";
+}
 ?>
